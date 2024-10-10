43bb6a52900f2e906e4dd58c313e5b8d8c4bbd1b
Tejas Karusala <tejaskarusala@icloud.com>
2024-10-10 19:29:21 -0400

    fixed db

--- utils/schema.js
+++ utils/schema.js
@@ -4,7 +4,7 @@ export const MockInterview=pgTable('mockInterview', {
     id: serial('id').primaryKey(),
     jsonMockResp:text('jsonMockResp').notNull(),
     jobPosition: varchar('jobPosition').notNull(),
-    jobDesc: varchar('jobDesc').notNull(),
+    jobDescription: varchar('jobDesc').notNull(),
     jobExperience: varchar('jobExperience').notNull(),
     createdBy: varchar('createdBy').notNull(),
     createdAt: varchar('createdAt').notNull(),
