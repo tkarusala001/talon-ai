a4402d52acd1efdf9a9f8d521eb34b938ebd5fc5
Tejas Karusala <tejaskarusala@icloud.com>
2024-10-11 17:05:32 -0400

    worked on ui

--- app/dashboard/interview/[interviewID]/page.jsx
+++ app/dashboard/interview/[interviewID]/page.jsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button'
 import { db } from '@/utils/db'
 import { MockInterview } from '@/utils/schema'
 import { eq } from 'drizzle-orm'
-import { WebcamIcon } from 'lucide-react'
+import { Lightbulb, WebcamIcon } from 'lucide-react'
 import React, { useEffect, useState} from 'react'
 import Webcam from 'react-webcam'
 
@@ -25,29 +25,44 @@ function Interview({params}) {
   return (
     <div className='my-10 flex justify-center flex-col items-center'>
         <h2 className='font-bold text-2xl'>Let's Get Started</h2>
-        <div>
-            {WebCamEnabled? <Webcam
-            onUserMedia={()=> setWebCamEnabled(true)}
-            onUserMediaError={() => setWebCamEnabled(false)}
-            mirrored={true}
-            style={{
-                height:300,
-                width:300
-            }} />
-            : 
-            <>
-            <WebcamIcon className='h-72 w-72 my-7 bg-secondary rounded-lg border'/>
-            <Button className='bg-purple-500 hover:bg-purple-200 flex justify-center mt-4' onClick={()=> setWebCamEnabled(true)}>Enable Web Cam and Microphone</Button>
-            </>
-        }
-        </div>
-        
-        <div className='flex flex-col my-5 gap-5'>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-10'>
+    
+        <div className='flex flex-col my-5 gap-5 '>
+            <div className='p-5 rounded-lg border'>
         <h2 className='text-lg'><strong>Job Role/Position:</strong> {interviewData ? interviewData.jobPosition : 'Loading...'}</h2>
         <h2 className='text-lg'><strong>Job Description:</strong> {interviewData ? interviewData.jobDescription : 'Loading...'}</h2>
         <h2 className='text-lg'><strong>Years of Experience:</strong> {interviewData ? interviewData.jobExperience : 'Loading...'}</h2>
         </div>
-    </div>
+        <div className='p-5 border rounded-lg border-yellow-300 bg-yellow-100'>
+            <h2 className='flex gap-2 items-center text-yellow-500'><Lightbulb/><strong>Information</strong></h2>
+            <h2>{process.env.NEXT_PUBLIC_INFORMATION}</h2>
+        </div>
+        </div>
+
+        <div>
+    {WebCamEnabled ? (
+        <Webcam
+            onUserMedia={() => setWebCamEnabled(true)}
+            onUserMediaError={() => setWebCamEnabled(false)}
+            mirrored={true}
+            style={{
+                height: 300,
+                width: 300,
+            }}
+        />
+    ) : (
+        <div className='grid grid-rows-2 justify-items-center'>
+            <WebcamIcon className='h-72 w-full my-7 bg-secondary rounded-lg border' />
+            <Button className='bg-purple-500 hover:bg-purple-200 mt-4' onClick={() => setWebCamEnabled(true)}>
+                Enable Web Cam and Microphone
+            </Button>
+        </div>
+    )}
+</div>
+
+        </div>
+    
+        </div>
     
   )
 }
