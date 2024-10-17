807b88b285d73c888d9310c507559aaf38b22320
Tejas Karusala <tejaskarusala@icloud.com>
2024-10-17 04:19:02 -0400

    updated

--- app/page.js
+++ app/page.js
@@ -6,13 +6,16 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
+    // Replace with your production signup URL
+    const signupUrl = 'https://talon-ai.vercel.app/sign-in?redirect_url=https%3A%2F%2Ftalon-ai.vercel.app%2Fdashboard';
+    
     // Redirect to the signup URL when the component mounts
-    router.push('http://localhost:3000/sign-in?redirect_url=http%3A%2F%2Flocalhost%3A3000%2Fdashboard');
+    router.push(signupUrl);
   }, [router]);
 
   return (
     <div>
-    
+      {/* Other components or content */}
     </div>
   );
 }
